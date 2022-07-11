9d5856a5e543085e21edd567b81bbb09a76409c7
Barış <102523196+qeaceq@users.noreply.github.com>
2022-07-11 16:29:18 +0300

    Add files via upload

---
+++ uygulama.py
@@ -0,0 +1,47 @@
+echo "# rockpaperscissors" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/qeaceq/rockpaperscissors.git
+git push -u origin main
+
+import random
+import tkinter
+from tkinter import simpledialog
+
+ROOT = tkinter.Tk()
+ROOT.withdraw()
+C = simpledialog.askstring(title="Taş Kağıt Makas",
+                                  prompt="Seçiminiz:")
+A = ["Taş","Kağıt","Makas"]
+B = random.choice(A)
+if C == B:
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Berabere, Bilgisayarın Seçimi:{B}")
+if C == "Taş" and B == "Kağıt":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Bilgisayar Kazandı, Bilgisayarın Seçimi:{B}")
+if C == "Taş" and B == "Makas":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Kazandınız!, Bilgisayarın Seçimi:{B}")
+if C == "Kağıt" and B == "Makas":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Bilgisayar Kazandı, Bilgisayarın Seçimi:{B}")
+if C == "Kağıt" and B == "Taş":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Kazandınız!, Bilgisayarın Seçimi:{B}")
+if C == "Makas" and B == "Taş":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Bilgisayar Kazandı, Bilgisayarın Seçimi:{B}")
+if C == "Makas" and B == "Kağıt":
+    tkinter.messagebox.showwarning(title="Sonuç", message=f"Kazandınız!,Bilgisayarın Seçimi:{B}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
